00886495b982523f974f5e03732b1b9ff7e33ffb
lucascarmotinoco@gmail.com <lucascarmotinoco@gmail.com>
2022-12-08 11:47:36 +0000

    adicionei stg e criei todas dimensoes faltantes

--- models/staging/erp_northwind/stg_erp__transportadoras.sql
+++ models/staging/erp_northwind/stg_erp__transportadoras.sql
@@ -1,11 +1,10 @@
 with
     source_transportadoras as (
-        select 
+        select
             cast(shipper_id as int) as id_transportadora
-            , cast(company_name as string) as nome_da_transportadora		
-            , cast(phone as string) as telefone		
-        from {{ source ('erp','shippers') }}
+            , cast(company_name as string) as nome_transportadora
+            , cast(phone as string) as telefone_transportadora
+        from {{ source('erp', 'shippers') }}
     )
-
-select * 
+select *
 from source_transportadoras
